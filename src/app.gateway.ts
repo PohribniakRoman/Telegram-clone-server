43a2fd60a260c198c0f33dc0e5bf04021ea492da
Roman Pohribniak <roman.pohribniak@gmail.com>
2022-06-10 13:13:47 +0300

    added chat

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,13 +1,38 @@
-import { SubscribeMessage,WebSocketGateway,OnGatewayInit } from "@nestjs/websockets";
+import { SubscribeMessage,WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+const db = []
+
 
 @WebSocketGateway()
-export class AppGateway implements OnGatewayInit {
-    afterInit(server: any) {
-        console.log(server);
-        
+export class AppGateway  {
+    @WebSocketServer()
+    server;
+    @SubscribeMessage("joinToRoom")
+    handleJoin(client:any,data:{user1,user2}){
+        let connected = false;
+        const separator = "12-\\-12";
+        const createRoom = data.user1+separator+data.user2
+        console.log(createRoom);
+        db.forEach(room=>{
+            if (room.id.split(separator).includes(data.user1) && room.id.split(separator).includes(data.user2)) {
+                client.join(room.id)
+                connected = true;   
+            }
+        })
+        if(!connected){
+            client.join(createRoom)
+            db.push({id:createRoom,msg:[]})
+        }
+        console.log(db);
     }
+
     @SubscribeMessage("msgToClient")
-    handleMsg(client:any,payload:any){
-        console.log(client.id)
+    handleMsg(client:any,data:{sender,room,msg}){
+        db.forEach(room=>{
+            if(room.id === data.room){
+                this.server.to(data.room).emmit("msg",{sender:data.sender,msg:data.msg}) 
+                room.msg.push({sender:data.sender,msg:data.msg})
+            }
+        })
     }
 }
