38ad20f58f39b39d438b8e55fa99e3cf2848af4c
Roman <roman.pohribniak@gmail.com>
2022-11-08 21:41:52 +0200

    add profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppGateway } from './app.gateway';
 import { UserModule } from './auth/auth.module';
 import { LoginModule } from './login/login.module';
+import { ProfileModule } from './profile/profile.module';
 import { SearchModule } from './search/search.module';
 import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb+srv://Admin:admin@cluster0.rb5jf.mongodb.net/?retryWrites=true&w=majority"),UserModule,LoginModule,SessionModule,SearchModule],
+  imports: [MongooseModule.forRoot("mongodb+srv://Admin:admin@cluster0.rb5jf.mongodb.net/?retryWrites=true&w=majority"),UserModule,LoginModule,SessionModule,SearchModule,ProfileModule],
   providers:[AppGateway]
 })
 export class AppModule {}
---
+++ src/profile/profile.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Profile, ProfileSchema } from "src/schemas/profile.schema";
+import { ProfileController } from "./profile.controller";
+import { ProfileService } from "./profile.service";
+
+@Module({
+    imports:[MongooseModule.forFeature([{name:Profile.name,schema:ProfileSchema}])],
+    controllers:[ProfileController],
+    providers:[ProfileService]
+})
+export class ProfileModule {};
